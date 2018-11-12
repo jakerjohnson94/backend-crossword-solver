d590ffca0b8064ee433534fc88be14b2d440b4ba
Jakerjohnson94 <jacobrichardjohnson94@gmail.com>
2018-11-12 10:27:29 -0500

    init, completed.

--- xword.py
+++ xword.py
@@ -2,9 +2,22 @@
 # -*- coding: utf-8 -*-
 """Crossword Solver Program"""
 
-__author__ = "???"
+__author__ = "Jake Johnson"
 
 # YOUR HELPER FUNCTION GOES HERE
+
+
+def find_possible_words(test_word, words):
+    result = []
+    trash = []
+    for word in words:
+        for index, letter in enumerate(test_word):
+            if len(word) == len(test_word):
+                if word[index] == letter or letter == ' ':
+                    result.append(word)
+                else:
+                    trash.append(word)
+    return sorted(list(set([x for x in result if x not in trash])))
 
 
 def main():
@@ -14,8 +27,8 @@ def main():
     test_word = raw_input(
         'Please enter a word to solve.\nUse spaces to signify unknown letters: ').lower()
 
-    # YOUR ADDITIONAL CODE HERE
-    raise NotImplementedError('Please complete this')
+    for word in find_possible_words(test_word, words):
+        print(word)
 
 
 if __name__ == '__main__':
